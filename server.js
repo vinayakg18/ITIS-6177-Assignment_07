6a4c5548b341f092df1c4508083efb8199a3b7cd
vinayakg18 <vinayakraddi18@gmail.com>
2024-10-06 14:42:28 -0400

    changes

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const PORT = 3000;
 app.get('/say', async (req, res) => {
   try {
     const keyword = req.query.keyword;
-    // Correct API Gateway URL with /say path
     const response = await axios.get('https://y99ksugyd0.execute-api.us-east-1.amazonaws.com/Prod/say', {
-      params: { keyword },  // Passing the keyword as a query parameter
+      params: { keyword },  
     });
 
-    res.send(response.data); // Send the response back to the client
+    res.send(response.data); 
   } catch (error) {
-    console.log(error); // Log the error for debugging
-    res.status(500).send('Error: Keyword not found'); // Handle the error response
+    console.log(error); 
+    res.status(500).send('Error: Keyword not found'); 
   }
 });
 
